c52cdbaa2269c741f2f55225fc3cc2382028dfee
Matt DeBoer <matt.deboer@gettyimages.com>
2016-08-31 13:09:19 -0700

    Initial import

---
+++ test/test_offer_filter.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+TEST(OfferFilter, CanQueryEndpoint)
+{
+
+  // TODO: wait for the mesos api to become available, and
+  // then hit the API with a filter, then attempt to provision
+  // an application on that host using marathon; finally,
+  //
+
+}
+
+
